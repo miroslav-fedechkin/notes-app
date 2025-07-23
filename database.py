74973d9cd30fa6bb8dcce24604c5048d97bdcede
Miroslav <fedechkin.miroslav@yandex.ru>
2025-07-23 17:18:20 +0200

    first commit

---
+++ database.py
@@ -0,0 +1,29 @@
+from typing import Optional
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+
+engine = create_async_engine(
+    'sqlite+aiosqlite:///tasks.db'
+)
+
+new_session = async_sessionmaker(engine, expire_on_commit=False)
+
+class Base(DeclarativeBase):
+    pass
+
+class TaskOrm(Base):
+    __tablename__ = 'tasks'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    description = Column(String, nullable=True)
+
+
+async def create_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(lambda sync_conn: Base.metadata.create_all(bind=sync_conn))
+
+async def delete_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(lambda sync_conn: Base.metadata.drop_all(bind=sync_conn))
